ca142f2b47359823856f7e435e5a7957a20c04df
flinger-bit <xdffsxdxd@gmail.com>
2026-04-15 20:44:43 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,13 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GameLevelManager.hpp>
 
+using namespace geode::prelude;
+
+class $modify(NoOrbs, GameLevelManager) {
+
+    int getRewardForLevel(GJGameLevel* level, bool secretCoin, bool dailyLevel, bool gauntlet) {
+
+        // Bloquea completamente las orbes
+        return 0;
+    }
+};
